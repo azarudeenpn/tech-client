e661f32ed48a06667ca5671a72882e060aa8066f
Aslam Rasheed <aslampr07@outlook.com>
2019-02-28 12:26:25 +0530

    Registration UI is ready

--- app/src/main/java/com/admin/tech/client/SignUpActivity.kt
+++ app/src/main/java/com/admin/tech/client/SignUpActivity.kt
@@ -1,15 +1,12 @@
 package com.admin.tech.client
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SignUpActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_main)
-        val i = Intent(this, SignUpActivity::class.java)
-        startActivity(i)
+        setContentView(R.layout.activity_sign_up)
     }
 }
